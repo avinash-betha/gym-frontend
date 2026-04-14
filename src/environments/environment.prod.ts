ddd51e76296a13753a9990661cf03eb7214d36e1
avinash-betha <152133994+avinash-betha@users.noreply.github.com>
2026-04-14 16:55:14 -0400

    Update apiBaseUrl for production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: '',
+  apiBaseUrl: 'https://gym.avkdevapps.com',
   supabaseUrl: 'https://vhantekmwdraudovjqih.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZoYW50ZWttd2RyYXVkb3ZqcWloIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwNDYyNTYsImV4cCI6MjA5MTYyMjI1Nn0.mZCJCVbxvniJHYZTBfvxhMH9lx-anw_QvqtGY_qvg4Y'
 };
